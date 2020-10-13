2c09d66ce4d3bc188d01957ee195decf7c9ba5e2
Daniel Ferecatu <ferecatu.daniel@gmail.com>
2020-10-14 01:26:37 +0300

    Added project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace df_netcore_weboptimizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
