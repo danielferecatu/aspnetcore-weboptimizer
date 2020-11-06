1805a66455301a96fc787ce3b62aac41105f9b81
Daniel Ferecatu <ferecatu.daniel@gmail.com>
2020-11-06 22:02:00 +0200

    The forgotten commit

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -13,17 +14,38 @@ namespace df_netcore_weboptimizer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services
+                .AddControllersWithViews()
+                .AddRazorRuntimeCompilation()
+                .AddViewLocalization(options => options.ResourcesPath = "Resources");
+            ;       
+
+            if (!Env.IsDevelopment())
+            {
+                services.AddWebOptimizer(minifyJavaScript:false,minifyCss:false);
+            }
+            else {
+                services.AddWebOptimizer(pipeline =>
+                {
+                    pipeline.CompileScssFiles();
+                    pipeline.MinifyCssFiles();
+
+                    pipeline.AddFiles("application/javascript", "**/*.js")
+                        .Localize<Strings>();
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +62,20 @@ namespace df_netcore_weboptimizer
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+
+            var cultures = new List<CultureInfo>
+            {
+                new CultureInfo("en"),
+                new CultureInfo("da")
+            };
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                SupportedCultures = cultures,
+                SupportedUICultures = cultures
+            });
+
+            app.UseWebOptimizer();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -53,5 +89,10 @@ namespace df_netcore_weboptimizer
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+    }
+
+    public class Strings
+    {
+
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function foo() {
+    var localizedString = "{{hello}}";
+
+    return localizedString;
+}
